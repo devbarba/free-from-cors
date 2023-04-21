ab6d4df4190e9025f8f01b50f195089eda640bdd
Joao Harbs <joao.harbs@mutuiamo.it>
2023-04-21 14:12:57 +0200

    project finish

---
+++ README.md
@@ -0,0 +1,9 @@
+# free-from-cors
+
+Simple and lightweight cors proxy with express
+
+
+## Instalation
+
+ - npm install
+ - npm run start
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const port = 3000;
+const app = express();
+
+// CORS Middleware
+app.use(cors());
+
+// Proxy Middleware
+app.use(createProxyMiddleware({
+    router: (req) => new URL(req.path.substring(1)),
+    pathRewrite: (req) => (new URL(req.path.substring(1))).pathname,
+    changeOrigin: true,
+    logger: console
+}));
+
+app.listen(port, () => console.log(`Free From Cors at port ${port}`));
